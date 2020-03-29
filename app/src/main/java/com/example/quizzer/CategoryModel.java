21ae58bcb8e59ccd69a4ec6b0df72f962e6a89d1
Anni1123 <kumarianni1213@gmail.com>
2020-03-29 15:10:48 +0530

    SecondCommit

---
+++ app/src/main/java/com/example/quizzer/CategoryModel.java
@@ -0,0 +1,4 @@
+package com.example.quizzer;
+
+public class CategoryModel {
+}
--- app/src/main/java/com/example/quizzer/MainActivity.java
+++ app/src/main/java/com/example/quizzer/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.quizzer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button start,bookmark;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start=(Button)findViewById(R.id.start_btn);
+        bookmark=(Button)findViewById(R.id.bookmark_btn);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CategoryActivity.class));
+            }
+        });
     }
 }
