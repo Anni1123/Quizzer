ff7287f8b624184bd36244e49f6a4f79682ea20a
Anni1123 <kumarianni1213@gmail.com>
2020-03-29 15:38:05 +0530

    CategoryActivity

---
+++ app/src/main/java/com/example/quizzer/CategoryAdapter.java
@@ -0,0 +1,57 @@
+package com.example.quizzer;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Viewholder> {
+    public CategoryAdapter(List<CategoryModel> categoryModels) {
+        this.categoryModels = categoryModels;
+    }
+
+    private List<CategoryModel> categoryModels;
+
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item,parent,false);
+        return new Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+        holder.setData(categoryModels.get(position).getImageurl(),categoryModels.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryModels.size();
+    }
+
+    class Viewholder extends RecyclerView.ViewHolder{
+        private CircleImageView circleImageView;
+        private TextView textView;
+
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+            circleImageView=(CircleImageView)itemView.findViewById(R.id.image_view);
+            textView=itemView.findViewById(R.id.title);
+
+        }
+        private void setData(String url,String title){
+            Glide.with(itemView.getContext()).load(url).into(circleImageView);
+            this.textView.setText(title);
+        }
+    }
+}
