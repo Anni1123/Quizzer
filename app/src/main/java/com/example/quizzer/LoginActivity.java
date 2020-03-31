47b0aa40f1bf6d3f3b27d12a85e8b2d4ac0ed327
Anni1123 <kumarianni1213@gmail.com>
2020-03-31 20:29:06 +0530

    LoginActivity

--- app/src/main/java/com/example/quizzer/LoginActivity.java
+++ app/src/main/java/com/example/quizzer/LoginActivity.java
@@ -1,14 +1,62 @@
 package com.example.quizzer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email,password;
+    private Button login;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.loginprogress);
+        firebaseAuth=FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().equals("")){
+                    email.setError("required");
+                    return;
+                }
+                else {
+                    email.setError(null);
+                }
+                if(password.getText().toString().equals("")){
+                   password.setError("required");
+                   return;
+                }
+                else {
+                   password.setError(null);
+                }
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this,"Done",Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
