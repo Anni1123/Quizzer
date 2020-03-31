473ce4c3753dcc8053a428556486eafd4dfd0d4f
Anni1123 <kumarianni1213@gmail.com>
2020-03-31 17:28:28 +0530

    LoginActivity

--- app/src/main/java/com/example/quizzer/MainActivity.java
+++ app/src/main/java/com/example/quizzer/MainActivity.java
@@ -4,18 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button start,bookmark;
+    private Button start,bookmark,login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         start=(Button)findViewById(R.id.start_btn);
         bookmark=(Button)findViewById(R.id.bookmark_btn);
+        login=(Button)findViewById(R.id.loginm);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
