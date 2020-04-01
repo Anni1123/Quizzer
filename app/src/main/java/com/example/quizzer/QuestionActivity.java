94913dcabb7a85f1c04424a8300f4e183242789e
Anni1123 <kumarianni1213@gmail.com>
2020-04-01 15:22:49 +0530

    QuestionActivityAdmin

--- app/src/main/java/com/example/quizzer/QuestionActivity.java
+++ app/src/main/java/com/example/quizzer/QuestionActivity.java
@@ -6,9 +6,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,13 +28,21 @@ public class QuestionActivity extends AppCompatActivity {
     private Button add,excel;
     private RecyclerView recyclerView;
     private QuestionAdapter adapter;
+    private Dialog load;
+    private List<QuestionsModel> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         Toolbar toolbar=findViewById(R.id.toolbar4);
-        String name=getIntent().getStringExtra("category");
-        int set=getIntent().getIntExtra("setNo",1);
+        load=new Dialog(this);
+
+        load.setContentView(R.layout.loading);
+        load.getWindow().setBackgroundDrawable(getDrawable(R.drawable.rounded_corner));
+        load.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        load.setCancelable(false);
+        final String name=getIntent().getStringExtra("category");
+        final int set=getIntent().getIntExtra("setNo",1);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(name+"/set"+set);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -34,17 +52,19 @@ public class QuestionActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager=new LinearLayoutManager(this);
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
-       List<QuestionsModel> list=new ArrayList<>();
-       list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
-        list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
-        list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
-        list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
-        list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
-        list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
-        list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
-        list.add(new QuestionsModel("abcc","Question?","a","b","c","d","a",set));
+         list=new ArrayList<>();
         adapter=new QuestionAdapter(list);
         recyclerView.setAdapter(adapter);
+        getData(name,set);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addQuestion=new Intent(QuestionActivity.this,AddNewQuestionActivity.class);
+                addQuestion.putExtra("category",name);
+                addQuestion.putExtra("set",set);
+                startActivity(addQuestion);
+            }
+        });
     }
 
     @Override
@@ -54,4 +74,33 @@ public class QuestionActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private void getData(String name, final int set){
+        load.show();
+        FirebaseDatabase.getInstance().getReference().child("SETS").child(name).child("questions").orderByChild("setNo").equalTo(set).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                    String id=dataSnapshot1.getKey();
+                    String question=dataSnapshot1.child("question").getValue().toString();
+                    String a=dataSnapshot1.child("a").getValue().toString();
+                    String b=dataSnapshot1.child("b").getValue().toString();
+                    String c=dataSnapshot1.child("c").getValue().toString();
+                    String d=dataSnapshot1.child("d").getValue().toString();
+                    String correctans=dataSnapshot1.child("correctans").getValue().toString();
+                    list.add(new QuestionsModel(id,question,a,b,c,d,correctans,set));
+                }
+                load.dismiss();
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(QuestionActivity.this,"Error",Toast.LENGTH_LONG).show();
+                load.dismiss();
+                finish();
+            }
+        });
+    }
+
 }
